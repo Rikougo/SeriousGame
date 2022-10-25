f6dd5c23f17924f284ae0f83c04b5c01481e7808
Elye <samhelye@gmail.com>
2022-10-25 15:15:18 +0200

    Sync camcontroller in tutorial

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,8 +1,6 @@
 using UnityEngine;
 public class CameraController : MonoBehaviour
 {
-    
-    [SerializeField] private float speed;
     private float currentPosX;
     private Vector3 velocity = Vector3.zero;
 
